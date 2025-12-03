2e8dc234c9151aa27631e19c629afd3744b0d379
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2025-12-03 01:46:16 -0500

    Update AppRoutes.tsx

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,6 +5,8 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Step11 from "../components/formSteps/Step11";
 import Home from "../pages/Home";
+import PlanDiario from "../pages/PlanDiario";
+import ProyeccionObjetivo from "../pages/ProyeccionObjetivo";
 
 export default function AppRoutes() {
   return (
@@ -13,8 +15,17 @@ export default function AppRoutes() {
       <Route path="/Login" element={<Login />}></Route>
       <Route path="/Register" element={<Register />}></Route>
       <Route path="/Form" element={<Form />}></Route>
-      <Route path="/Step11" element={<Step11 />}></Route>
       <Route path="/Home" element={<Home />}></Route>
+
+      {/* Pantalla para dirigir al register */}
+      <Route path="/Step11" element={<Step11 />}></Route>
+      {/*  */}
+
+      <Route path="/PlanDiario" element={<PlanDiario />}></Route>
+      <Route
+        path="/ProyeccionObjetivo"
+        element={<ProyeccionObjetivo />}
+      ></Route>
     </Routes>
   );
 }
