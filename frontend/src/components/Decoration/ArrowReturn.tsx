5359bdb89f78f3c44173e0e7e38b176f13638ebb
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-01-23 15:11:40 -0500

    Update ArrowReturn.tsx

--- frontend/src/components/Decoration/ArrowReturn.tsx
+++ frontend/src/components/Decoration/ArrowReturn.tsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
 
-export default function ArrowReturn({ to = "/" }) {
+interface Props {
+  to?: string;
+  onClick?: () => void;
+}
+
+export default function ArrowReturn({ to = "/", onClick }: Props) {
+  const handleClick = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    if (onClick) {
+      event.preventDefault();
+      onClick();
+    }
+  };
+
   return (
     <div className="absolute top-5 left-5 z-10 w-10">
-      <Link to={to}>
+      <Link to={to} onClick={handleClick}>
         <img
           src="/SVG/IconsGeneral/ArrowReturn.svg"
           alt="Volver al inicio"
