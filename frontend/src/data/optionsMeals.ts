80b908f40f280469442ab976de9a949773949d2d
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-01-23 15:46:36 -0500

    Create optionsMeals.ts

---
+++ frontend/src/data/optionsMeals.ts
@@ -0,0 +1,34 @@
+import type { Meal } from "../types";
+
+export const optionsMeals: Meal[] = [
+    {
+        label: "Desayuno",
+        icon: "/SVG/Platos/Dark/DesayunoDark.svg",
+        selectedIcon: "/SVG/Platos/Desayuno.svg",
+    },
+    {
+        label: "Snack1",
+        icon: "/SVG/Platos/Dark/SnackDark.svg",
+        selectedIcon: "/SVG/Platos/Snack.svg",
+    },
+    {
+        label: "Almuerzo",
+        icon: "/SVG/Platos/Dark/AlmuerzoDark.svg",
+        selectedIcon: "/SVG/Platos/Almuerzo.svg",
+    },
+    {
+        label: "Snack2",
+        icon: "/SVG/Platos/Dark/SnackDark.svg",
+        selectedIcon: "/SVG/Platos/Snack.svg",
+    },
+    {
+        label: "Cena",
+        icon: "/SVG/Platos/Dark/CenaDark.svg",
+        selectedIcon: "/SVG/Platos/Cena.svg",
+    },
+    {
+        label: "Snack3",
+        icon: "/SVG/Platos/Dark/SnackDark.svg",
+        selectedIcon: "/SVG/Platos/Snack.svg",
+    },
+];
