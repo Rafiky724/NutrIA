b8ece54515ad19e9af0b07beaf959938601085a4
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-01-23 21:15:59 -0500

    Create planService.ts

---
+++ frontend/src/services/planService.ts
@@ -0,0 +1,27 @@
+import axiosClient from "../api/axiosClient";
+import { PLAN_ENDPOINTS } from "../api/endpoints";
+import type { Macros } from "../types";
+
+export const PlanService = {
+
+    getDailyMacros: async () => {
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                throw new Error("Usuario no autenticado");
+            }
+
+            const { data } = await axiosClient.get<Macros>(PLAN_ENDPOINTS.MACROS_DIARIOS, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+            return data;
+        } catch (error: any) {
+            console.error("Error obteniendo macronutrientes diarios:", error);
+            throw error;
+        }
+    },
+};
