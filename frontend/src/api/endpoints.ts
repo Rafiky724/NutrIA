0042733294e395382c5e838e8c238e74f5525681
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-01-23 23:11:14 -0500

    Update endpoints.ts

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -2,3 +2,31 @@ export const AUTH_ENDPOINTS = {
   REGISTER: "/auth/register",
   LOGIN: "/auth/login"
 } as const;
+
+export const DIET_ENDPOINTS = {
+  CREATE: "/dieta/create",
+  START: "/dieta/iniciar",
+} as const;
+
+export const PLAN_ENDPOINTS = {
+  MACROS_DAILY: "/plan/macros",
+} as const;
+
+export const OBJECTIVE_ENDPOINTS = {
+  DATES: "/objetivo/fechas",
+} as const;
+
+export const DAYS_ENDPOINTS = {
+  BY_NAME: (day: string) => `/dias/${day}`,
+
+  EDIT_FOOD: (day: string, typeFood: string) =>
+    `/dias/${day}/comidas/${typeFood}/editar`,
+
+  REGENERATE_FOOD: (day: string, typeFood: string) =>
+    `/dias/${day}/comidas/${typeFood}/regenerar`,
+
+} as const;
+
+export const USER_ENDPOINTS = {
+  HAS_PLAN: "/user/tiene-plan",
+} as const;
