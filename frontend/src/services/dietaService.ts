23d891b25408b5c3afc41bb2afb2533c455ea996
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-01-23 23:19:07 -0500

    Create dietaService.ts

---
+++ frontend/src/services/dietaService.ts
@@ -0,0 +1,59 @@
+import axiosClient from "../api/axiosClient";
+import { DIET_ENDPOINTS } from "../api/endpoints";
+
+export const DietService = {
+
+    createDiet: async (): Promise<string> => {
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                throw new Error("Usuario no autenticado");
+            }
+
+            const { data } = await axiosClient.get<string>(DIET_ENDPOINTS.CREATE, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+            return data;
+        } catch (error: any) {
+            console.error("Error creando dieta:", error);
+            throw error;
+        }
+    },
+
+    iniciarDieta: async (payload: {
+        tipo_inicio: "hoy" | "manana" | "fecha";
+        siguiente_comida?: string;
+        fecha_inicio?: string;
+        dia_actualizar_dieta: string;
+    }): Promise<string> => {
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                throw new Error("Usuario no autenticado");
+            }
+
+            const { data } = await axiosClient.post<string>(
+                DIET_ENDPOINTS.START,
+                payload,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            return data;
+        } catch (error: any) {
+            console.error("Error iniciando dieta:", error.response?.data || error.message);
+            throw error;
+        }
+    },
+
+};
+
+
