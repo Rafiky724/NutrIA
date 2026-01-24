dbc3892bebd225f1acfa0f25da63b7eb9dbe9c6f
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-01-23 23:13:18 -0500

    Update AppRoutes.tsx

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,31 +1,48 @@
 import { Route, Routes } from "react-router-dom";
-import Form from "../pages/Form";
-import Init from "../pages/Init";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import Step11 from "../components/formSteps/Step11";
+import Form from "../pages/Form/Form";
+import Init from "../pages/Home/Init";
+import Login from "../pages/Auth/Login";
+import FinalForm from "../components/FormSteps/FinalForm";
+import DailyNutritionPlan from "../pages/Plan/DailyNutritionPlan";
+import GoalProjection from "../pages/Plan/GoalProjection";
+import RouteProtection from "../components/Protected/RouteProtection";
+import WeeklyMealPlan from "../pages/Plan/WeeklyMealPlan";
+import DietCreationReady from "../pages/Onboarding/DietCreationReady";
+import DietPlanReady from "../pages/Onboarding/DietPlanReady";
 import Home from "../pages/Home";
-import PlanDiario from "../pages/PlanDiario";
-import ProyeccionObjetivo from "../pages/ProyeccionObjetivo";
+import StartDiet from "../pages/Onboarding/StartDiet";
+import UpdateDietDay from "../pages/Onboarding/UpdateDietDay";
+import LoadingPage from "../pages/Loading/Loading";
+import Register from "../pages/Auth/Register";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Rutas públicas */}
+      <Route path="/loading" element={<LoadingPage />}></Route>
       <Route path="/" element={<Init />}></Route>
-      <Route path="/Login" element={<Login />}></Route>
-      <Route path="/Register" element={<Register />}></Route>
-      <Route path="/Form" element={<Form />}></Route>
-      <Route path="/Home" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/form" element={<Form />}></Route>
+      <Route path="/finalForm" element={<FinalForm />}></Route>
+      <Route path="/register" element={<Register />}></Route>
 
-      {/* Pantalla para dirigir al register */}
-      <Route path="/Step11" element={<Step11 />}></Route>
-      {/*  */}
-
-      <Route path="/PlanDiario" element={<PlanDiario />}></Route>
-      <Route
-        path="/ProyeccionObjetivo"
-        element={<ProyeccionObjetivo />}
-      ></Route>
+      {/* Rutas protegidas */}
+      <Route element={<RouteProtection />}>
+        <Route
+          path="/dietCreationReady"
+          element={<DietCreationReady />}
+        ></Route>
+        <Route path="/dietPlanReady" element={<DietPlanReady />}></Route>
+        <Route
+          path="/dailyNutritionPlan"
+          element={<DailyNutritionPlan />}
+        ></Route>
+        <Route path="/goalProjection" element={<GoalProjection />}></Route>
+        <Route path="/weeklyMealPlan" element={<WeeklyMealPlan />}></Route>
+        <Route path="/startDiet" element={<StartDiet />}></Route>
+        <Route path="/updateDietDay" element={<UpdateDietDay />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Route>
     </Routes>
   );
 }
