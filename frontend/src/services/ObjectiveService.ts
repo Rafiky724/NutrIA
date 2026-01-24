64a97b97d3d9be7406be3c3676e73fdd2561318a
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-01-23 23:23:54 -0500

    Create ObjectiveService.ts

---
+++ frontend/src/services/ObjectiveService.ts
@@ -0,0 +1,30 @@
+import axiosClient from "../api/axiosClient";
+import { OBJECTIVE_ENDPOINTS } from "../api/endpoints";
+import type { DatesTargetResponse } from "../types";
+
+export const ObjectiveService = {
+
+    getTargetDates: async (): Promise<DatesTargetResponse> => {
+        try {
+            const token = localStorage.getItem("token");
+
+            if (!token) {
+                throw new Error("Usuario no autenticado");
+            }
+
+            const { data } = await axiosClient.get<DatesTargetResponse>(
+                OBJECTIVE_ENDPOINTS.DATES,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            return data;
+        } catch (error) {
+            console.error("Error obteniendo fechas del objetivo:", error);
+            throw error;
+        }
+    },
+};
