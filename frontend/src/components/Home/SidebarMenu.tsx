f4d464a46e4daf773a535f7f7974a7e568865d95
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-07 13:58:48 -0500

    Create SidebarMenu.tsx

---
+++ frontend/src/components/Home/SidebarMenu.tsx
@@ -0,0 +1,34 @@
+import type { Dispatch, SetStateAction } from "react";
+import { menuItems } from "../../data/menuItems";
+
+type Props = {
+  active: number;
+  setActive: Dispatch<SetStateAction<number>>;
+};
+
+export default function SidebarMenu({ active, setActive }: Props) {
+  return (
+    <div className=" bg-white rounded-tr-3xl rounded-br-3xl p-3 shadow-md fixed top-50 left-0">
+      <div className="flex flex-col gap-6 -ml-2">
+        {menuItems.map((item, idx) => {
+          const isActive = idx === active;
+          const theme = isActive ? "Light" : "Dark";
+          const iconSrc = `/SVG/Menu/${theme}/${item.nombre}.svg`;
+
+          return (
+            <div
+              key={idx}
+              className={
+                "mx-auto cursor-pointer w-10 h-10 flex items-center justify-center"
+              }
+              onClick={() => setActive(idx)}
+              title={item.alt}
+            >
+              <img src={iconSrc} alt={item.alt} className="w-5 h-5" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
