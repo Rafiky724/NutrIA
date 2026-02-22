a1bae620e77e992a543b5deb9eb01f8a7bb7b11b
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-22 16:44:03 -0500

    Update axiosClient.ts

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json"
   }
