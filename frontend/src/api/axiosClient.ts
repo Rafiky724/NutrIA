66ead851c13c678feae2f122c819ffd9d38acd17
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-22 19:25:55 -0500

    Update axiosClient.ts

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,12 +1,11 @@
 import axios, { AxiosError } from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json"
   }
 });
-
 axiosClient.interceptors.response.use(
   response => response,
   (error: AxiosError) => {
