fed7c11e330c251c667d22341d376b4d0fe2018c
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-24 20:44:22 -0500

    Update ActivityLevelForm.tsx

--- frontend/src/components/FormSteps/ActivityLevelForm.tsx
+++ frontend/src/components/FormSteps/ActivityLevelForm.tsx
@@ -29,26 +29,26 @@ export default function ActivityLevelForm({
     <div className="px-4 sm:px-6 md:px-10">
       {/* Encabezado */}
       <div className="flex flex-col sm:flex-row items-center justify-center sm:space-x-6 space-y-4 sm:space-y-0">
-        <div className="w-16 sm:w-20">
+        <div className="w-10 md:w-20">
           <img
             src="/SVG/IconsGeneral/WeightIcon.svg"
             alt="Datos Actividad Física"
             className="w-full h-auto"
           />
         </div>
-        <div className="ft-bold text-xl sm:text-2xl text-brown text-center sm:text-left">
+        <div className="ft-bold text-lg sm:text-xl md:text-2xl text-brown text-center md:text-left">
           <h2>¿Realizas actividad física?</h2>
         </div>
       </div>
 
       {/* Descripción */}
-      <div className="mt-6 sm:mt-8 text-justify ft-light text-gray text-sm sm:text-base px-2 sm:px-6">
+      <div className="ft-light text-justify text-gray my-6 md:my-10 text-sm sm:text-base md:text-md px-2 sm:px-6">
         Tu nivel de actividad física es clave para calcular tu gasto calórico y
         ajustar tus porciones
       </div>
 
       {/* Botones de selección */}
-      <div className="flex flex-col gap-3 mt-6 sm:mt-8">
+      <div className="flex flex-col gap-2 mt-6 sm:mt-8">
         {[
           { label: "No hago ejercicio", value: "NoHace" },
           { label: "Ocasional (1-2 veces por semana)", value: "Bajo" },
@@ -59,7 +59,7 @@ export default function ActivityLevelForm({
             key={option.value}
             type="button"
             onClick={() => handleSelect(option.value as LevelActivity)}
-            className="w-full sm:w-80 md:w-96 mx-auto py-3 rounded-3xl cursor-pointer ft-medium text-md sm:text-lg custom-bg transition hover:scale-105"
+            className="w-full sm:w-80 md:w-96 mx-auto py-2 rounded-3xl cursor-pointer ft-medium text-sm sm:text-lg custom-bg transition hover:scale-105"
           >
             <h4 className="text-center px-4">{option.label}</h4>
           </button>
