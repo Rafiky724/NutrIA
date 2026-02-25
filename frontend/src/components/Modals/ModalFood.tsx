51bf3e2e7b84733f8f0094a6ef15084dd5ef5415
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-25 18:39:36 -0500

    Update ModalFood.tsx

--- frontend/src/components/Modals/ModalFood.tsx
+++ frontend/src/components/Modals/ModalFood.tsx
@@ -18,9 +18,9 @@ export default function ModalFood({
   if (!show) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+    <div className="fixed inset-0  flex items-center justify-center z-50 p-4 bg-black/60 backdrop-blur-sm">
       <div className="w-full max-w-md sm:max-w-lg md:max-w-xl bg-white rounded-3xl p-6 shadow-lg flex flex-col gap-5">
-        <p className="text-md sm:text-lg text-justify text-gray px-5">
+        <p className="text-gray text-md sm:text-base md:text-lg text-justify px-2 sm:px-6">
           Como empiezas hoy, cuéntanos cuál será tu próxima comida y nos
           adaptamos a ti.
         </p>
@@ -44,7 +44,7 @@ export default function ModalFood({
         <div className="flex justify-end gap-3 mt-4">
           <button
             onClick={onClose}
-            className="px-5 py-2 rounded-full bg-yellow ft-medium w-full sm:w-auto mx-auto cursor-pointer"
+            className="w-3xs sm:w-xs bg-yellow text-brown font-semibold py-2 rounded-full shadow text-md mx-auto cursor-pointer hover:scale-105 transition"
           >
             Aceptar
           </button>
