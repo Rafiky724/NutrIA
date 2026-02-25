fc9053de42a040c9624294ce7e9f74e03b3f5454
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-24 23:49:37 -0500

    Update DishCard.tsx

--- frontend/src/components/WeeklyMealPlan/DishCard.tsx
+++ frontend/src/components/WeeklyMealPlan/DishCard.tsx
@@ -19,33 +19,33 @@ export default function DishCard({
   const { protein, carbs, fats } = calculateMacroPercentages(dish);
 
   return (
-    <div className="bg-input p-4 sm:p-6 md:p-8 rounded-2xl flex flex-col lg:flex-row gap-6 w-full max-w-4xl mx-auto">
+    <div className="bg-input p-4 rounded-2xl flex flex-col lg:flex-row gap-6 w-full max-w-4xl mx-auto">
       {/* IZQUIERDA */}
-      <div className="flex flex-col items-center justify-center gap-4 lg:w-1/3">
-        <div className="w-full max-w-xs sm:max-w-sm md:max-w-full object-cover">
+      <div className="flex flex-col items-center justify-center gap-4 lg:w-4xs">
+        <div className="w-full object-cover">
           <img
             src="/SVG/ejemploPlato.jpeg"
             alt="Plato"
-            className="rounded-3xl w-full h-auto"
+            className="w-auto h-auto"
           />
         </div>
 
-        <div className="flex flex-col sm:flex-row justify-between w-full px-2 text-brown ft-medium text-xs sm:text-sm md:text-base">
-          <div className="flex flex-col items-center sm:items-start mb-2 sm:mb-0">
+        <div className="flex flex-col sm:flex-row justify-between w-full px-2 text-brown ft-medium text-sm md:text-xs">
+          <div className="flex flex-col items-center mb-2">
             <span>
               {dish.proteinas} g ({protein.toFixed(0)}%)
             </span>
             <span className="ft-light mt-1">Proteína</span>
             <div className="w-12 h-1 bg-yellow-500 mt-1"></div>
           </div>
-          <div className="flex flex-col items-center sm:items-start mb-2 sm:mb-0">
+          <div className="flex flex-col items-center mb-2">
             <span>
               {dish.carbohidratos} g ({carbs.toFixed(0)}%)
             </span>
             <span className="ft-light mt-1">Carbohidratos</span>
             <div className="w-12 h-1 bg-yellow-700 mt-1"></div>
           </div>
-          <div className="flex flex-col items-center sm:items-start">
+          <div className="flex flex-col items-center">
             <span>
               {dish.grasas} g ({fats.toFixed(0)}%)
             </span>
@@ -63,10 +63,10 @@ export default function DishCard({
       </div>
 
       {/* DERECHA */}
-      <div className="flex flex-col lg:w-2/3 gap-3 w-full">
+      <div className="flex flex-col w-full gap-3">
         <div className="flex justify-between items-center">
-          <h2 className="text-md ft-bold text-brown">Ingredientes</h2>
-          <span className="text-gray ft-medium text-sm sm:text-base">
+          <h2 className="text-sm ft-bold text-brown">Ingredientes</h2>
+          <span className="text-gray ft-medium text-sm">
             ≈ ${dish.precio_estimado} COP
           </span>
         </div>
@@ -103,7 +103,7 @@ export default function DishCard({
           <div className="flex flex-col sm:flex-row gap-3 mt-3 sm:mt-6 w-full">
             <button
               onClick={onEdit}
-              className="relative bg-yellow text-brown py-3 rounded-3xl px-6 ft-medium shadow text-center w-full sm:w-auto cursor-pointer"
+              className="relative bg-yellow text-brown py-3 rounded-3xl px-6 ft-medium shadow text-center w-full sm:w-auto cursor-pointer hover:scale-105 transition"
             >
               Editar ingredientes
               <div className="absolute top-3 right-5 w-6">
@@ -117,7 +117,7 @@ export default function DishCard({
             <button
               onClick={onRegenerate}
               disabled={loading}
-              className="relative bg-brown text-white ft-medium py-3 rounded-3xl px-6 shadow w-full sm:w-auto cursor-pointer"
+              className="relative bg-brown text-white ft-medium py-3 rounded-3xl px-6 shadow w-full sm:w-auto cursor-pointer hover:scale-105 transition"
             >
               Regenerar plato
               <div className="absolute top-3 right-5 w-6">
