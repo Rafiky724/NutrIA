bba39336d00528cbf8d690ec5da01f02888b8f48
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-26 19:59:26 -0500

    Update NavBar.tsx

--- frontend/src/components/Home/NavBar.tsx
+++ frontend/src/components/Home/NavBar.tsx
@@ -8,34 +8,40 @@ export default function NavBar({ user }: Props) {
   return (
     <>
       <nav className="flex justify-center md:justify-between items-center">
-        <div>
-          <h1 className="text-2xl ft-bold text-brown">Hola, {user.nombre}</h1>
-          <p className="text-sm ft-medium text-gray">¿Cómo va tu día?</p>
+        <div className="mr-4 md:mr-0">
+          <h1 className="text-sm md:text-2xl ft-bold text-brown">
+            Hola, {user.nombre}
+          </h1>
+          <p className="text-xs md:text-sm ft-medium text-gray">
+            ¿Cómo va tu día?
+          </p>
         </div>
         <div className="flex items-center gap-4">
           <div className="relative flex gap-4">
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1 hover:scale-105 transition cursor-pointer">
               <img
                 src="/SVG/IconsGeneral/FireStreak.svg"
                 alt="Racha"
-                className="w-6 h-6"
+                className="w-4 md:w-6 h-4 md:h-6"
               />
-              <p className="ft-bold text-2xl">{user.numero_racha}</p>
+              <p className="ft-bold text-md md:text-2xl">{user.numero_racha}</p>
             </div>
-            <div className="flex items-center gap-1">
+            <div className="flex items-center gap-1 hover:scale-105 transition cursor-pointer">
               <img
                 src="/SVG/IconsGeneral/GemsIcon.svg"
                 alt="Gemas"
-                className="w-6 h-6"
+                className="w-4 md:w-6 h-4 md:h-6"
               />
-              <p className="ft-bold text-2xl">{user.cantidad_gemas}</p>
+              <p className="ft-bold text-md md:text-2xl">
+                {user.cantidad_gemas}
+              </p>
             </div>
           </div>
 
           <img
             src="/Background/LogoIcono.png"
             alt="Usuario"
-            className="w-12 h-12"
+            className="w-12 h-12 hover:scale-105 transition cursor-pointer"
           />
         </div>
       </nav>
