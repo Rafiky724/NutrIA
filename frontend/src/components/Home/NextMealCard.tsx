f794ebac0a864b4082ac09bed7a83a043cbb6e2c
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-27 22:35:45 -0500

    Update NextMealCard.tsx

--- frontend/src/components/Home/NextMealCard.tsx
+++ frontend/src/components/Home/NextMealCard.tsx
@@ -8,30 +8,30 @@ type Props = {
 export default function NextMealCard({ nextFood }: Props) {
   return (
     <>
-      <div className="flex-1 bg-white rounded-3xl p-6 shadow flex flex-col gap-4 ml-10 w-2xs md:w-sm h-95">
+      <div className="w-2xs md:w-lg xl:w-4xl h-95 bg-white rounded-3xl p-4 shadow flex flex-col gap-4 ml-10 md:ml-0 items-center">
         <h2 className="text-brown ft-bold text-lg text-center">
           Próxima comida - {nextFood?.hora_sugerida.toLowerCase()}
         </h2>
 
-        <div className="flex gap-4 overflow-hidden flex-col md:flex-row">
+        <div className="w-full flex gap-4 overflow-hidden flex-col md:flex-row">
           <div className="flex flex-col items-center gap-4">
             <img
               src="/SVG/ejemploPlato.jpeg"
               alt="Próxima comida"
               className="w-60 h-50 object-cover rounded-xl"
             />
-            <span className="text-gray ft-medium">
+            <span className="text-gray ft-medium text-sm md:text-md">
               {nextFood?.tipo_comida} ≈ ${nextFood?.precio_estimado} COP
             </span>
           </div>
 
-          <div className="flex flex-col flex-1">
-            <div className="text-sm text-brown ft-medium mb-2 text-center">
+          <div className="flex flex-4 flex-col">
+            <div className="text-xs md:text-sm text-brown ft-medium mb-2 text-center">
               {nextFood?.calorias} kcal | {nextFood?.proteinas} P |{" "}
               {nextFood?.carbohidratos} C | {nextFood?.grasas} G
             </div>
 
-            <div className="flex-1 overflow-y-auto pr-2 max-h-50">
+            <div className="overflow-y-auto max-h-50">
               {nextFood?.ingredientes.map((item, idx) => (
                 <div
                   key={idx}
@@ -40,9 +40,9 @@ export default function NextMealCard({ nextFood }: Props) {
                   <img
                     src={getIngredientIcon(item.nombre)}
                     alt={item.nombre}
-                    className="w-6 h-6"
+                    className="w-5 md:w-6 h-5 md:h-6"
                   />
-                  <span className="text-gray text-sm">
+                  <span className="text-gray text-xs md:text-md">
                     {item.cantidad} {item.nombre}
                   </span>
                 </div>
