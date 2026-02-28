6456462e377df443bdbbc3dd766b2dc9e6cc9a4e
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-02-27 23:03:09 -0500

    Update HomeLayout.tsx

--- frontend/src/pages/Home/HomeLayout.tsx
+++ frontend/src/pages/Home/HomeLayout.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Home from "./Home";
 import SidebarMenu from "../../components/Home/SidebarMenu";
 import Diet from "./Diet";
+import Pantry from "./Pantry";
+import Config from "./Config";
 
 export default function HomeLayout() {
   const [active, setActive] = useState(0);
@@ -11,10 +13,9 @@ export default function HomeLayout() {
       <SidebarMenu active={active} setActive={setActive} />
 
       {active === 0 && <Home />}
-      {active === 1 && <Diet />}
+      {active === 1 && <Pantry />}
       {active === 2 && <Diet />}
-      {active === 3 && <Diet />}
-      {active === 4 && <Diet />}
+      {active === 3 && <Config />}
     </>
   );
 }
