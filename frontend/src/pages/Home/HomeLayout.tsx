98799550e70709eaf553ca1d8a0c5c5cd6ad4fee
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-01 13:59:56 -0500

    Update HomeLayout.tsx

--- frontend/src/pages/Home/HomeLayout.tsx
+++ frontend/src/pages/Home/HomeLayout.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import Home from "./Home";
 import SidebarMenu from "../../components/Home/SidebarMenu";
 import Diet from "./Diet";
@@ -6,7 +7,15 @@ import Pantry from "./Pantry";
 import Config from "./Config";
 
 export default function HomeLayout() {
+  const [searchParams] = useSearchParams();
+  const sectionFromURL = Number(searchParams.get("section"));
   const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    if (!isNaN(sectionFromURL)) {
+      setActive(sectionFromURL);
+    }
+  }, [sectionFromURL]);
 
   return (
     <>
