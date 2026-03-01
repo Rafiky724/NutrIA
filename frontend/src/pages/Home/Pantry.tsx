fbe8bedf1a24bd48c4165af1bc4124b479e5da50
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-01 09:17:51 -0500

    Update Pantry.tsx

--- frontend/src/pages/Home/Pantry.tsx
+++ frontend/src/pages/Home/Pantry.tsx
@@ -38,7 +38,7 @@ export default function Pantry() {
     <div className="flex min-h-screen bg-input pl-0 md:pl-20 pr-0 md:pr-10">
       <div className="flex-1 py-6 flex flex-col gap-6">
         <div className="block md:hidden">
-          <NavBar user={homeData?.usuario} />
+          <NavBar user={homeData?.usuario} subtitle="Estas en la despensa" />
         </div>
         <div className="hidden md:block">
           <NavBar
@@ -47,50 +47,10 @@ export default function Pantry() {
           />
         </div>
 
-        <div className="flex flex-col md:flex-row items-start justify-center p-4 md:p-6 gap-6">
-          {/* DESPENSA ACTUAL */}
-          <div className="bg-white rounded-2xl shadow-md p-6 w-full md:w-1/2 max-h-[600px] overflow-y-auto">
-            <h3 className="ft-bold text-lg text-brown mb-2">
-              Tu despensa actual
-            </h3>
-
-            {pantry.length === 0 && (
-              <p className="text-gray text-sm">
-                No tienes ingredientes agregados.
-              </p>
-            )}
-
-            {categories.map((cat) => {
-              const items = pantry.filter((i) => i.categoria === cat.nombre);
-              if (items.length === 0) return null;
-
-              return (
-                <div key={cat.nombre} className="p-2">
-                  <h4 className="ft-medium text-brown mb-3">{cat.nombre}</h4>
-                  <div className="flex flex-wrap gap-3">
-                    {items.map((ingredient) => (
-                      <button
-                        key={ingredient.nombre}
-                        onClick={() => toggleIngredient(ingredient)}
-                        className="bg-input px-3 py-2 rounded-xl shadow-sm hover:scale-105 transition flex items-center gap-2 cursor-pointer ft-light"
-                      >
-                        {ingredient.nombre}
-                        <img
-                          src={ingredient.icono}
-                          alt={ingredient.nombre}
-                          className="w-5 h-5 object-contain"
-                        />
-                      </button>
-                    ))}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-
+        <div className="flex gap-4 md:gap-6 flex-col md:flex-row items-center justify-center">
           {/* INGREDIENTES DISPONIBLES */}
-          <div className="bg-white rounded-2xl shadow-md p-6 w-full max-h-[600px] flex flex-col">
-            <h3 className="ft-bold text-lg text-brown mb-2">
+          <div className="flex flex-col bg-white rounded-3xl p-4 md:p-6 shadow gap-4 ml-10 w-2xs md:ml-0 h-95 md:w-1/3 md:h-[550px] xl:h-[600px] max-h-[600px] overflow-y-auto">
+            <h3 className="ft-bold text-lg text-brown mb-0 md:mb-2">
               Agregar nuevos ingredientes
             </h3>
 
@@ -134,10 +94,50 @@ export default function Pantry() {
             </div>
 
             <div className="flex justify-center">
-              <button className="w-full sm:w-72 md:w-80 bg-yellow text-brown ft-medium px-4 py-2 rounded-3xl hover:scale-105 transition cursor-pointer">
+              <button className="w-full sm:w-72 md:w-80 bg-yellow text-brown ft-medium py-2 rounded-3xl hover:scale-105 transition cursor-pointer text-xs">
                 Guardar
               </button>
             </div>
+          </div>
+
+          {/* DESPENSA ACTUAL */}
+          <div className="flex flex-col bg-white rounded-3xl p-4 md:p-6 shadow gap-4 ml-10 w-2xs md:ml-0 h-95 md:w-2/2 md:h-[550px] xl:h-[600px] max-h-[600px] overflow-y-auto">
+            <h3 className="ft-bold text-lg text-brown mb-0 md:mb-2">
+              Tu despensa actual
+            </h3>
+
+            {pantry.length === 0 && (
+              <p className="text-gray text-sm">
+                No tienes ingredientes agregados.
+              </p>
+            )}
+
+            {categories.map((cat) => {
+              const items = pantry.filter((i) => i.categoria === cat.nombre);
+              if (items.length === 0) return null;
+
+              return (
+                <div key={cat.nombre} className="p-2">
+                  <h4 className="ft-medium text-brown mb-3">{cat.nombre}</h4>
+                  <div className="flex flex-wrap gap-3">
+                    {items.map((ingredient) => (
+                      <button
+                        key={ingredient.nombre}
+                        onClick={() => toggleIngredient(ingredient)}
+                        className="bg-input px-3 py-2 rounded-xl shadow-sm hover:scale-105 transition flex items-center gap-2 cursor-pointer ft-light"
+                      >
+                        {ingredient.nombre}
+                        <img
+                          src={ingredient.icono}
+                          alt={ingredient.nombre}
+                          className="w-5 h-5 object-contain"
+                        />
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
