f7ce4d679fea1d2ffc426cab268b37bc3af5a1fe
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-03 21:46:46 -0500

    Update SidebarMenu.tsx

--- frontend/src/components/Home/SidebarMenu.tsx
+++ frontend/src/components/Home/SidebarMenu.tsx
@@ -1,27 +1,23 @@
-import type { Dispatch, SetStateAction } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { menuItems } from "../../data/menuItems";
 
-type Props = {
-  active: number;
-  setActive: Dispatch<SetStateAction<number>>;
-};
+export default function SidebarMenu() {
+  const navigate = useNavigate();
+  const location = useLocation();
 
-export default function SidebarMenu({ active, setActive }: Props) {
   return (
-    <div className=" bg-white rounded-tr-3xl rounded-br-3xl p-3 shadow-md fixed top-50 left-0 z-50">
+    <div className="bg-white rounded-tr-3xl rounded-br-3xl p-3 shadow-md fixed top-50 left-0 z-50">
       <div className="flex flex-col gap-6 -ml-2">
         {menuItems.map((item, idx) => {
-          const isActive = idx === active;
+          const isActive = location.pathname === item.path;
           const theme = isActive ? "Light" : "Dark";
           const iconSrc = `/SVG/Menu/${theme}/${item.nombre}.svg`;
 
           return (
             <div
               key={idx}
-              className={
-                "mx-auto cursor-pointer w-8 h-8 md:w-10 md:h-10 flex items-center justify-center"
-              }
-              onClick={() => setActive(idx)}
+              className="mx-auto cursor-pointer w-8 h-8 md:w-10 md:h-10 flex items-center justify-center"
+              onClick={() => navigate(item.path)}
               title={item.alt}
             >
               <img src={iconSrc} alt={item.alt} className="w-5 h-5" />
