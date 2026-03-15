5f3ca77f76ee14e20d91eb1543076a2ad90f4081
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-15 13:47:54 -0500

    Update axiosClient.ts

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -6,9 +6,16 @@ const axiosClient = axios.create({
     "Content-Type": "application/json"
   }
 });
+
 axiosClient.interceptors.response.use(
-  response => response,
+  (response) => response,
   (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+
+      window.location.href = "/login";
+    }
+
     return Promise.reject(error);
   }
 );
