97049a0298113b8ee4e6dc0443848668adb99382
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-15 18:02:47 -0500

    Update menu.ts

--- frontend/src/data/menu.ts
+++ frontend/src/data/menu.ts
@@ -25,21 +25,68 @@ export const editFields: EditField[] = [
 ];
 
 export const shopItemsData = [
-    { name: "Item 1", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Item 2", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Item 3", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Item 4", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Item 5", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Item 6", icon: "/SVG/Pets/Adopt/Cat.svg" },
+    { name: "nutria", icon: "/SVG/Pets/Shop/Mascotas/nutria.svg", category: "mascotas", price: 23, available: true },
+    { name: "perro", icon: "/SVG/Pets/Shop/Mascotas/perro.svg", category: "mascotas", price: 23, available: true },
+    { name: "gato", icon: "/SVG/Pets/Shop/Mascotas/gato.svg", category: "mascotas", price: 23, available: false },
+    { name: "dinosaurio", icon: "/SVG/Pets/Shop/Mascotas/dinosaurio.svg", category: "mascotas", price: 23, available: true },
+
+    { name: "gorro1", icon: "/SVG/Pets/Shop/Gorros/gorra1.svg", category: "gorros", price: 23, available: true },
+    { name: "gorro2", icon: "/SVG/Pets/Shop/Gorros/gorra2.svg", category: "gorros", price: 23, available: false },
+    { name: "gorro3", icon: "/SVG/Pets/Shop/Gorros/gorra3.svg", category: "gorros", price: 23, available: true },
+    { name: "gorro4", icon: "/SVG/Pets/Shop/Gorros/gorra4.svg", category: "gorros", price: 23, available: true },
+    { name: "gorro5", icon: "/SVG/Pets/Shop/Gorros/gorra5.svg", category: "gorros", price: 23, available: true },
+    { name: "gorro6", icon: "/SVG/Pets/Shop/Gorros/gorra6.svg", category: "gorros", price: 23, available: true },
+
+    { name: "gafas1", icon: "/SVG/Pets/Shop/Gafas/gafas1.svg", category: "gafas", price: 23, available: true },
+    { name: "gafas2", icon: "/SVG/Pets/Shop/Gafas/gafas2.svg", category: "gafas", price: 23, available: true },
+    { name: "gafas3", icon: "/SVG/Pets/Shop/Gafas/gafas3.svg", category: "gafas", price: 23, available: false },
+    { name: "gafas4", icon: "/SVG/Pets/Shop/Gafas/gafas4.svg", category: "gafas", price: 23, available: true },
+    { name: "gafas5", icon: "/SVG/Pets/Shop/Gafas/gafas5.svg", category: "gafas", price: 23, available: true },
+    { name: "gafas6", icon: "/SVG/Pets/Shop/Gafas/gafas6.svg", category: "gafas", price: 23, available: true },
+
+    { name: "fondos1", icon: "/SVG/Pets/Shop/Fondos/fondo1.svg", category: "fondos", price: 23, available: true },
+    { name: "fondos2", icon: "/SVG/Pets/Shop/Fondos/fondo2.svg", category: "fondos", price: 23, available: true },
+    { name: "fondos3", icon: "/SVG/Pets/Shop/Fondos/fondo3.svg", category: "fondos", price: 23, available: false },
+    { name: "fondos4", icon: "/SVG/Pets/Shop/Fondos/fondo4.svg", category: "fondos", price: 23, available: true },
+    { name: "fondos5", icon: "/SVG/Pets/Shop/Fondos/fondo5.svg", category: "fondos", price: 23, available: true },
+    { name: "fondos6", icon: "/SVG/Pets/Shop/Fondos/fondo6.svg", category: "fondos", price: 23, available: true },
+
+    { name: "marcos1", icon: "/SVG/Pets/Shop/Marcos/marco1.svg", category: "marcos", price: 23, available: true },
+    { name: "marcos2", icon: "/SVG/Pets/Shop/Marcos/marco2.svg", category: "marcos", price: 23, available: true },
+    { name: "marcos3", icon: "/SVG/Pets/Shop/Marcos/marco3.svg", category: "marcos", price: 23, available: false },
+    { name: "marcos4", icon: "/SVG/Pets/Shop/Marcos/marco4.svg", category: "marcos", price: 23, available: true },
+    { name: "marcos5", icon: "/SVG/Pets/Shop/Marcos/marco5.svg", category: "marcos", price: 23, available: true },
+    { name: "marcos6", icon: "/SVG/Pets/Shop/Marcos/marco6.svg", category: "marcos", price: 23, available: true },
+
+    { name: "accesorios1", icon: "/SVG/Pets/Shop/Accesorios/accesorio1.svg", category: "accesorios", price: 23, available: true },
+    { name: "accesorios2", icon: "/SVG/Pets/Shop/Accesorios/accesorio2.svg", category: "accesorios", price: 23, available: true },
+    { name: "accesorios3", icon: "/SVG/Pets/Shop/Accesorios/accesorio3.svg", category: "accesorios", price: 23, available: false },
+    { name: "accesorios4", icon: "/SVG/Pets/Shop/Accesorios/accesorio4.svg", category: "accesorios", price: 23, available: true },
 ];
 
 export const categoriesData = [
-    { name: "Pet", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Hat", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Glasses", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Background", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Frame", icon: "/SVG/Pets/Adopt/Cat.svg" },
-    { name: "Tie", icon: "/SVG/Pets/Adopt/Cat.svg" },
+    { name: "Mascotas", icon: "/SVG/Pets/Shop/Category/Mascotas.svg", type: "mascotas" },
+    {
+        name: "Gorras",
+        icon: "/SVG/Pets/Shop/Category/Gorras.svg",
+        type: "gorros",
+    },
+    { name: "Gafas", icon: "/SVG/Pets/Shop/Category/Gafas.svg", type: "gafas" },
+    {
+        name: "Fondos",
+        icon: "/SVG/Pets/Shop/Category/Fondos.svg",
+        type: "fondos",
+    },
+    {
+        name: "Marcos",
+        icon: "/SVG/Pets/Shop/Category/Marcos.svg",
+        type: "marcos",
+    },
+    {
+        name: "Accesorios",
+        icon: "/SVG/Pets/Shop/Category/Accesorios.svg",
+        type: "accesorios",
+    },
 ];
 
 export const contactData = {
