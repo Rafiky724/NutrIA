27895572bd38b5ede09d5c1399cc02eca34aad03
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-22 01:59:59 -0500

    Update ModalWeightTarget.tsx

--- frontend/src/components/Modals/ModalWeightTarget.tsx
+++ frontend/src/components/Modals/ModalWeightTarget.tsx
@@ -11,7 +11,7 @@ export default function ModalWeightTarget({
   onSelectTargetWeight,
   onClose,
 }: Props) {
-  const weights = Array.from({ length: 171 }, (_, i) => i + 30); // 30 - 200 kg
+  const weights = Array.from({ length: 171 }, (_, i) => i + 30);
   const weightRoulette = useRoulette(weights.length, 40);
 
   const [toast, setToast] = useState({
@@ -20,7 +20,6 @@ export default function ModalWeightTarget({
     type: "error" as "error" | "success" | "warning" | "info",
   });
 
-  // Bloquear scroll del body mientras el modal está abierto
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
@@ -67,7 +66,7 @@ export default function ModalWeightTarget({
       <div
         className="flex flex-col items-center w-20 sm:w-24 select-none"
         onWheel={(e) => {
-          e.preventDefault(); // evita que el body se desplace
+          e.preventDefault();
           roulette.onWheel(e);
         }}
         onTouchStart={roulette.onTouchStart}
@@ -95,13 +94,11 @@ export default function ModalWeightTarget({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center px-4 sm:px-6 text-center">
-      {/* Fondo oscuro */}
       <div
         className="absolute inset-0 bg-black/60 backdrop-blur-sm"
         onClick={onClose}
       />
 
-      {/* Modal */}
       <div className="relative z-10 bg-white p-6 sm:p-8 rounded-2xl shadow-lg w-full max-w-md mx-auto">
         <h2 className="text-xl sm:text-2xl text-brown ft-bold mb-6 text-center">
           Selecciona tu peso objetivo
