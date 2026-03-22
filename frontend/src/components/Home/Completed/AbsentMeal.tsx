2bcf4aa8106169d221a5ae31a678fe5a938177cd
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-22 01:18:57 -0500

    Update AbsentMeal.tsx

--- frontend/src/components/Home/Completed/AbsentMeal.tsx
+++ frontend/src/components/Home/Completed/AbsentMeal.tsx
@@ -30,20 +30,20 @@ export default function AbsentMeal({
       onClick={onClose}
     >
       <div
-        className="bg-white rounded-4xl p-8 flex flex-col gap-6 w-2xl h-120"
+        className="bg-white rounded-4xl p-8 flex flex-col gap-6 w-xs md:w-2xl md:h-120"
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className="text-center text-2xl ft-bold text-brown">{title}</h2>
 
         <textarea
-          className="w-xl h-100 p-6 border-none rounded-4xl resize-none text-sm bg-input mx-auto placeholder:p-.5 placeholder:text-justify"
+          className="w-full md:w-xl h-100 p-6 border-none rounded-4xl resize-none text-sm bg-input mx-auto placeholder:p-.5 placeholder:text-justify"
           value={text}
           onChange={(e) => setText(e.target.value)}
           placeholder={description}
         />
 
         <button
-          className="w-xs mx-auto bg-yellow text-brown ft-medium py-2 px-4 rounded-4xl hover:scale-105 transition cursor-pointer"
+          className="w-3xs md:w-xs mx-auto bg-yellow text-brown ft-medium py-2 px-4 rounded-4xl hover:scale-105 transition cursor-pointer"
           onClick={handleSubmit}
         >
           Continuar
