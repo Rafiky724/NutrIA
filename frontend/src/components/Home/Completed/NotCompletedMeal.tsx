4dbd97daf726c7267d03179acdda888923ed7479
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-22 01:17:03 -0500

    Update NotCompletedMeal.tsx

--- frontend/src/components/Home/Completed/NotCompletedMeal.tsx
+++ frontend/src/components/Home/Completed/NotCompletedMeal.tsx
@@ -9,23 +9,23 @@ export default function NotCompletedMeal({ isOpen, onClose, onAction }: Props) {
 
   return (
     <div
-      className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+      className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-5"
       onClick={onClose}
     >
       <div
-        className="bg-white rounded-3xl p-8 pb-20 px-20 flex flex-col gap-8"
+        className="bg-white rounded-4xl p-8 md:pb-20 md:px-20 flex flex-col gap-4 md:gap-8 w-full md:w-xl"
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className="text-center text-lg ft-bold text-brown">
           Selecciona una opción
         </h2>
-        <p className="text-justify text-sm text-gray-700 w-md ft-light">
+        <p className="text-justify text-sm text-gray md:w-md ft-light">
           No pasa nada si hoy no pudiste completar tu comida. Crear nuevos
           hábitos toma tiempo. Si quieres, puedes recuperar tu racha usando
           gemas. Lo importante es no rendirse y seguir intentandolo.
         </p>
 
-        <div className="flex flex-col gap-4 w-xs mx-auto">
+        <div className="flex flex-col gap-4 md:w-xs mx-auto">
           <button
             className="bg-yellow text-brown ft-medium py-3 px-4 rounded-xl hover:scale-105 transition text-xs text-left cursor-pointer"
             onClick={() => onAction("retry")}
