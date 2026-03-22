32011859c50c0c2c15ffc497e8f2d7ad6c51de1e
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-22 01:14:08 -0500

    Update VerifyMeal.tsx

--- frontend/src/components/Home/Completed/VerifyMeal.tsx
+++ frontend/src/components/Home/Completed/VerifyMeal.tsx
@@ -32,23 +32,23 @@ export default function VerifyMeal({ isOpen, onClose }: Props) {
 
   return (
     <div
-      className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+      className="fixed inset-0 bg-black/40 flex items-center justify-center z-50 p-5"
       onClick={onClose}
     >
       <div
-        className="bg-white rounded-3xl p-10 flex flex-col gap-8 pb-25"
+        className="bg-white rounded-4xl p-10 flex flex-col md:gap-8 md:pb-25 w-full md:w-2xl max-h-150 overflow-y-auto items-center"
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="text-center text-lg ft-bold text-brown mb-10">
+        <h2 className="text-center text-lg ft-bold text-brown md:mb-10">
           Selecciona una opción
         </h2>
 
-        <div className="flex flex-col md:flex-row w-lg justify-center space-x-15">
+        <div className="flex flex-col md:flex-row md:w-lg justify-center space-x-15 md:gap-8">
           {options.map((opt) => (
             <button
               key={opt.id}
               onClick={() => setSelected(opt.id)}
-              className="bg-input rounded-4xl p-6 flex flex-col items-center gap-8 cursor-pointer transition w-[150px] h-[180px] hover:scale-105"
+              className="bg-input rounded-4xl p-6 flex flex-col items-center gap-8 cursor-pointer transition md:w-[150px] md:h-[180px] hover:scale-105 m-4 md:m-0"
             >
               <img src={opt.image} className="w-20" />
               <span className="text-sm ft-bold text-center text-brown">
