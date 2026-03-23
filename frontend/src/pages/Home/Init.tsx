2c969fe18f1e683c8f023bb94c0963bf82b304fa
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-23 17:41:53 -0500

    Update Init.tsx

--- frontend/src/pages/Home/Init.tsx
+++ frontend/src/pages/Home/Init.tsx
@@ -6,8 +6,7 @@ export default function Init() {
   return (
     <div className="relative w-full min-h-screen bg-[url('/Background/Back.png')] bg-cover bg-center overflow-hidden">
       <div className="min-h-screen flex flex-col items-center justify-center px-6 text-center">
-        {/* Logo */}
-        <div className="w-40 sm:w-52 md:w-64 2xl:w-80 mb-6">
+        <div className="w-45 sm:w-52 md:w-64 lg:w-80 2xl:w-100 mb-6">
           <img
             src="./Background/Logo.png"
             alt="Logo NutrIA"
@@ -15,13 +14,11 @@ export default function Init() {
           />
         </div>
 
-        {/* Texto */}
         <h3 className="text-gray text-lg sm:text-xl md:text-2xl ft-medium max-w-xs sm:max-w-md md:max-w-xl leading-relaxed">
           Tu asistente virtual personalizado para lograr tus objetivos
           nutricionales
         </h3>
 
-        {/* Botones */}
         <div className="mt-10 flex flex-col items-center w-full max-w-xs">
           <Link
             to="/form"
@@ -30,7 +27,7 @@ export default function Init() {
             Empezar
           </Link>
 
-          <small className="mt-5 text-gray ft-light opacity-95 text-sm">
+          <small className="mt-5 text-gray ft-light opacity-95 text-sm 2xl:text-xl">
             ¿Tienes cuenta?{" "}
             <Link to="/login" className="text-brown ft-medium hover:underline">
               Iniciar sesión
@@ -39,7 +36,6 @@ export default function Init() {
         </div>
       </div>
 
-      {/* Decoraciones */}
       <div className="absolute bottom-0 left-0 z-10 w-24 sm:w-40 md:w-52 2xl:w-80">
         <FruitLeft />
       </div>
