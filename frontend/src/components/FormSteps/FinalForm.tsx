0b7911f386064add3b03ad9f686549bd0c19ea12
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-23 19:43:15 -0500

    Update FinalForm.tsx

--- frontend/src/components/FormSteps/FinalForm.tsx
+++ frontend/src/components/FormSteps/FinalForm.tsx
@@ -1,22 +1,20 @@
 export default function FinalForm() {
   return (
-    <div className="flex flex-col items-center justify-center px-4 sm:px-6 md:px-8">
-      {/* Header con icono */}
+    <div className="sm:px-6 md:px-10 text-center">
       <div className="flex flex-col sm:flex-row items-center justify-center sm:space-x-4 space-y-2 sm:space-y-0 mb-4">
-        <div className="w-12 sm:w-14 md:w-16">
+        <div className="w-12 sm:w-16">
           <img
             src="/SVG/IconsGeneral/BoardIcon.svg"
             alt="Cantidad de comidas"
             className="w-full h-auto"
           />
         </div>
-        <div className="ft-bold text-lg sm:text-xl md:text-2xl text-brown text-center md:text-left">
+        <div className="ft-bold text-lg md:text-xl text-brown text-center md:text-left">
           <h2>Todo listo para empezar</h2>
         </div>
       </div>
 
-      {/* Texto descriptivo */}
-      <p className="ft-light text-justify text-gray my-6 md:my-8 text-sm sm:text-base md:text-md px-2 sm:px-6">
+      <p className="ft-light text-justify text-gray my-3 md:my-5 text-sm sm:text-base md:text-md px-8 sm:px-16 pt-10 pb-10">
         Antes de continuar, crea una cuenta. Así podremos guardar tu
         información, objetivos y progreso.
       </p>
