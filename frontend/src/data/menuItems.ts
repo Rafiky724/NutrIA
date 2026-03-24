78e08a743212811078d551ac6de1c52921737a44
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-03-24 15:32:24 -0500

    Update menuItems.ts

--- frontend/src/data/menuItems.ts
+++ frontend/src/data/menuItems.ts
@@ -1,6 +1,6 @@
 export const menuItems = [
     { nombre: "Home", alt: "Inicio", path: "/home" },
-    { nombre: "PyramidApple", alt: "Despensa", path: "/pantry" },
-    { nombre: "BoxFood", alt: "Dieta", path: "/diet" },
+    { nombre: "PyramidApple", alt: "Despensa", path: "/diet" },
+    { nombre: "BoxFood", alt: "Dieta", path: "/pantry" },
     { nombre: "Settings", alt: "Configuración", path: "/config" },
 ];
