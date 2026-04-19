0cd20878a33ecc393ca3a6f4f04ec49de359eeee
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-04-19 13:41:00 -0500

    Update ConfirmMeal.tsx

--- frontend/src/components/Home/Completed/ConfirmMeal.tsx
+++ frontend/src/components/Home/Completed/ConfirmMeal.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { completarComida } from "../../../services/comidaService";
 import { type Option } from "../../../types";
 import { useProgress } from "../../../Context/ProgressContext";
+import SpinnerOverlay from "../../Loading/SpinnerOverlay";
 
 type Props = {
   isOpen: boolean;
@@ -17,6 +18,7 @@ export default function ConfirmMeal({
   onSuccess,
 }: Props) {
   const [loading, setLoading] = useState(false);
+  const [, setToastOpen] = useState(false);
   const { refreshProgress } = useProgress();
 
   if (!isOpen || !option) return null;
@@ -30,6 +32,8 @@ export default function ConfirmMeal({
         comida_id: option.id,
       });
 
+      setToastOpen(true);
+
       onSuccess();
       await refreshProgress();
       onClose();
@@ -41,34 +45,40 @@ export default function ConfirmMeal({
   };
 
   return (
-    <div
-      className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
-      onClick={onClose}
-    >
+    <>
       <div
-        className="bg-white rounded-4xl p-10 px-35 flex flex-col items-center gap-6 w-[90%] max-w-lg"
-        onClick={(e) => e.stopPropagation()}
+        className="fixed inset-0 bg-black/40 flex items-center justify-center z-50"
+        onClick={onClose}
       >
-        <button className="text-brown ft-bold py-2 px-6 rounded-full  text-lg pb-10">
-          Confirmar
-        </button>
-
         <div
-          onClick={handleConfirm}
-          className="relative bg-input rounded-2xl p-4 flex flex-col items-center gap-4 w-3xs md:w-4xs hover:scale-105 transition cursor-pointer"
+          className="bg-white rounded-4xl p-10 px-35 flex flex-col items-center gap-6 w-[90%] max-w-lg"
+          onClick={(e) => e.stopPropagation()}
         >
-          <img src={option.image} className="w-22 mb-5" />
+          <button className="text-brown ft-bold py-2 px-6 rounded-full  text-lg pb-5">
+            Confirmar Comida
+          </button>
 
-          <span className="flex flex-col items-center">
-            <h2 className="text-sm ft-medium mb-2">He completado la comida</h2>
+          <div
+            onClick={handleConfirm}
+            className="relative bg-input rounded-2xl p-4 flex flex-col items-center gap-4 w-3xs md:w-4xs hover:scale-105 transition cursor-pointer"
+          >
+            <img src={option.image} className="w-22 mb-5" />
 
-            <p className="text-left text-xs text-gray ft-light">
-              ¡Tu avance depende de lo que registres.
-              <b className="ft-bold"> Hazlo con honestidad</b>!
-            </p>
-          </span>
+            <span className="flex flex-col items-center">
+              <h2 className="text-sm ft-medium mb-2">
+                He completado la comida
+              </h2>
+
+              <p className="text-left text-xs text-gray ft-light">
+                ¡Tu avance depende de lo que registres.
+                <b className="ft-bold"> Hazlo con honestidad</b>!
+              </p>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+
+      <SpinnerOverlay isOpen={loading} />
+    </>
   );
 }
