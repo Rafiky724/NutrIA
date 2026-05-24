97e3a96cecb768630c47896fd20ab0d1c19cb859
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-05-24 12:54:56 -0500

    Update DaySelector.tsx

--- frontend/src/components/WeeklyMealPlan/DaySelector.tsx
+++ frontend/src/components/WeeklyMealPlan/DaySelector.tsx
@@ -8,19 +8,31 @@ type Props = {
 };
 
 export default function DaySelector({ days, dayActive, setDayActive }: Props) {
+  const shortDays: Record<string, string> = {
+    Lunes: "L",
+    Martes: "M",
+    Miercoles: "M",
+    Jueves: "J",
+    Viernes: "V",
+    Sabado: "S",
+    Domingo: "D",
+  };
+
   return (
-    <div className="flex flex-col gap-2 md:flex-row justify-between mb-4">
+    <div className="flex flex-row gap-2 justify-between mb-4">
       {days.map((day) => (
         <button
           key={day}
           onClick={() => setDayActive(day)}
-          className={`px-4 md:px-10 py-1 rounded-4xl ft-medium transition-all ${
+          className={`px-3 sm:px-4 md:px-10 py-1 rounded-4xl ft-medium transition-all ${
             dayActive === day
               ? "bg-yellow text-brown shadow"
               : "bg-input text-gray"
-          } cursor-pointer text-sm md:text-md hover:scale-105 transition`}
+          } cursor-pointer text-sm md:text-md hover:scale-105`}
         >
-          {day}
+          <span className="block xl:hidden">{shortDays[day] || day}</span>
+
+          <span className="hidden xl:block">{day}</span>
         </button>
       ))}
     </div>
