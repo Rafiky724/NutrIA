a3f1d2f1b97c91b7653cda7b7354e6e12060ffc4
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2026-05-25 22:16:23 -0500

    Update HomeLayout.tsx

--- frontend/src/pages/Home/HomeLayout.tsx
+++ frontend/src/pages/Home/HomeLayout.tsx
@@ -1,8 +1,48 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import SidebarMenu from "../../components/Home/SidebarMenu";
+import LoadingScreen from "../../components/Loading/LoadingScreen";
+import { useEffect, useState } from "react";
+import { getHasPlan } from "../../services/userService";
 
 export default function HomeLayout() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkMascota = async () => {
+      try {
+        const data = await getHasPlan();
+
+        if (!data?.tiene_plan) {
+          navigate("/startDiet", { replace: true });
+          return;
+        }
+
+        if (!data?.tiene_mascota) {
+          navigate("/adoptMoment", { replace: true });
+          return;
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkMascota();
+  }, [navigate]);
+
+  if (loading) {
+    return (
+      <LoadingScreen
+        title="CARGANDO"
+        subtitle="Verificando mascota..."
+        loading={loading}
+      />
+    );
+  }
+
   return (
     <>
       <SidebarMenu />
